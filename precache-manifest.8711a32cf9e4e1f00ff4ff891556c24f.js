a59465cec48d9b3f44e0623c3d23dd5072f2f8b4
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2020-06-07 16:32:01 +0530

    Updates

---
+++ precache-manifest.8711a32cf9e4e1f00ff4ff891556c24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f88faf064c2f9012199da053f493ca7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "04071b6fedff600f4fb0",
+    "url": "/monsters-rolodex/static/css/main.fdbc7241.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04071b6fedff600f4fb0",
+    "url": "/monsters-rolodex/static/js/main.8c34e659.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
